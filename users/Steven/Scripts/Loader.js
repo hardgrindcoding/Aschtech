f3f4388007accfa9c94866357dfea09d86e3c401
Impwing <Steven.cederrand@icloud.com>
2018-11-11 12:10:38 +0100

    Minor changes

--- users/Steven/Scripts/Loader.js
+++ users/Steven/Scripts/Loader.js
@@ -1,7 +1,3 @@
-function start() {
-	console.log("Loaded Page");
-	window.location = document.getElementById('wp').href;
-}
 
 function loadSpecific(tag) {
 	document.getElementById(tag).innerHTML = 'Hello this javascript is working fine';
