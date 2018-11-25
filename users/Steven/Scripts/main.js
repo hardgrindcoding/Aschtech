5c6173716e67375d231d14955b1c1618531e2ec5
Impwing <steven.cederrand@icloud.com>
2018-11-25 15:40:01 +0100

    Polished the contact part of my page. Added secure https connection. Added removal of extensions to the page!

--- users/Steven/Scripts/main.js
+++ users/Steven/Scripts/main.js
@@ -11,14 +11,53 @@ function loadSpecific(tag) {
 
 function unfade(tag) {
 		var element = document.getElementById(tag);
-    var op = 0.01;  // initial opacity
-    element.style.display = 'block';
-    var timer = setInterval(function() {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 10);
+
+		if(element.style.display == "none") {
+			var op = 0.01;  // initial opacity
+	    element.style.display = 'block';
+	    var timer = setInterval(function() {
+	        if (op >= 1){
+	            clearInterval(timer);
+	        }
+	        element.style.opacity = op;
+	        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+	        op += op * 0.1;
+	    }, 10);
+		}
+}
+//The goal is to have this reverse the function above
+function fade(tag) {
+	var element = document.getElementById(tag);
+	element.style.display = "none";
+
+	var op = 1;  // initial opacity
+
+	var timer = setInterval(function() {
+			if (op < 0.1){
+					clearInterval(timer);
+			}
+			element.style.opacity = op;
+			element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+			op = op - 0.1;
+	}, 10);
+}
+
+function openContact() {
+	var element = document.getElementById('overlay');
+	element.style.display = "block";
+
+	var op = 0.01;  // initial opacity
+	var timer = setInterval(function() {
+			if (op >= 1){
+					clearInterval(timer);
+			}
+			element.style.opacity = op;
+			element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+			op += op * 0.1;
+	}, 10);
+}
+
+function closeContact() {
+
+
 }
