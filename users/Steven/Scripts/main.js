f3d7566970dd12a7e0c4a3eda18f14144389209e
Impwing <steven.cederrand@icloud.com>
2018-11-29 14:10:05 +0100

    Blur effect

--- users/Steven/Scripts/main.js
+++ users/Steven/Scripts/main.js
@@ -10,6 +10,7 @@ function loadSpecific(tag) {
 }
 
 function unfade(tag) {
+	
 		var element = document.getElementById(tag);
 
 		if(element.style.display == "none") {
@@ -24,9 +25,11 @@ function unfade(tag) {
 	        op += op * 0.1;
 	    }, 10);
 		}
+
 }
-//The goal is to have this reverse the function above
+//The goal is to reverse the 'fade' function
 function fade(tag) {
+
 	var element = document.getElementById(tag);
 	element.style.display = "none";
 
@@ -40,24 +43,18 @@ function fade(tag) {
 			element.style.filter = 'alpha(opacity=' + op * 100 + ")";
 			op = op - 0.1;
 	}, 10);
+
 }
 
-function openContact() {
-	var element = document.getElementById('overlay');
-	element.style.display = "block";
-
-	var op = 0.01;  // initial opacity
-	var timer = setInterval(function() {
-			if (op >= 1){
-					clearInterval(timer);
-			}
-			element.style.opacity = op;
-			element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-			op += op * 0.1;
-	}, 10);
+function logoBlur() {
+	var mainBackground = document.getElementById('wallpaper');
+	mainBackground.style.filter = "blur(8px)";
+	mainBackground.style.webkitFilter = "blur(8px)";
 }
 
-function closeContact() {
-
-
+function logoUnBlur() {
+	console.log("Something");
+	var mainBackground = document.getElementById('wallpaper');
+	mainBackground.style.filter = "blur(0px)";
+	mainBackground.style.webkitFilter = "blur(0px)";
 }
