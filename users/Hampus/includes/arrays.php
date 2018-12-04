cfc918ad802380467a50090f72d4708a4c6f1e6d
hampusehh <hampus.akesson@live.se>
2018-12-05 00:18:36 +0100

    fresh start

---
+++ users/Hampus/includes/arrays.php
@@ -0,0 +1,29 @@
+<?php
+    // Nav items
+
+    $navItems = array(
+                    array(
+                        slug =>"index.php",
+                        title => "Home"
+                    )
+                    array(
+                        slug =>"index.php",
+                        title => "Home"
+                    )
+                    array(
+                        slug =>"index.php",
+                        title => "Home"
+                    )
+                    array(
+                        slug =>"index.php",
+                        title => "Home"
+                    )
+                    array(
+                        slug =>"index.php",
+                        title => "Home"
+                    )
+
+    );
+
+)
+ ?>
---
+++ users/Hampus/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<style><?php include 'style/stylesheet.css';?></style> <!-- include for stylesheet -->
+	<head>
+		<title>My First PHP Web Page</title>
+		<link href="style/stylesheet.css" rel="stylesheet" type ="text/css"></link>
+	</head>
+	<body id="frontPage">
+		<div>
+			<?php print('Hello World!'); ?><p>
+		</div>
+		<div class="footer">
+			<footer>devSquad #169 2d018 devSquad Incdd. All rights reserved.</footer>
+		</div>
+	</body>
+</html>
